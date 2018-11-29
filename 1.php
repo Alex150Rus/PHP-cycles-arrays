dd2aec797d8fc8d99e969ad13c0514820763966b
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-11-29 09:27:03 +0300

    the first task (cycle - while) has been accomplished.

---
+++ 1.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 0;
+while ($a < 100) {
+    if ($a % 3 === 0) {
+    echo $a . ' ';
+    }
+    $a++;
+}
+
+
+/**
+ * Created by PhpStorm.
+ * User: Alex1
+ * Date: 29.11.2018
+ * Time: 9:17
+ */
